22133763a7e21f89876adb116723a5d86cad42e6
Tim <cpldcpu@gmail.com>
2022-01-11 05:50:12 +0100

    update to ne555 dice

--- 10_HDL/dice_johnson.vhd
+++ 10_HDL/dice_johnson.vhd
@@ -8,7 +8,7 @@ use ieee.numeric_std.all;
 entity dice555johnson is
 	port (clk:		in	std_logic;
 	      n_clk:	in	std_logic;
-		  nreset:	in std_logic;
+		  nrst:	in std_logic;
 		  dice: out std_logic_vector(3 downto 0)
 	);
 end;
@@ -16,12 +16,22 @@ end;
 architecture main of dice555johnson is
     signal  cnt:	unsigned(2 downto 0);
 begin
- 	process (clk,n_clk,nreset)
+ 	process (clk,n_clk,nrst)
 	begin
-		if nreset = '0' then
+		-- sync reset implementation to allow using yosys internal digital simulation
+		-- if rising_edge(clk) then 
+		-- 	if nrst = '0' then
+		-- 		cnt <= "000";
+		-- 	else
+        --     	cnt <= cnt(1 downto 0) & NOT cnt(2);
+		-- 	end if;
+		-- end if;
+
+		-- async reset for synthesis		
+		if nrst = '0' then
 			cnt <= "000";
-		elsif rising_edge(clk) then
-            cnt <= cnt(1 downto 0) & NOT cnt(2);
+		elsif rising_edge(clk) then 
+			cnt <= cnt(1 downto 0) & NOT cnt(2);
 		end if;
 	end process;
 
@@ -30,17 +40,17 @@ begin
 --  2     1
 
 -- Encoding:
--- 001  0
--- 010  1
--- 011  0,1
--- 100  1,2
--- 101  0,1,2
--- 110  1,2,3
+-- 011  0
+-- 000  1
+-- 001  0,1
+-- 110  1,2
+-- 111  0,1,2
+-- 100  1,2,3
 
 -- drive inverted LEDs
 dice(0) <= NOT cnt(0);
 dice(1) <= '1' when (cnt = "011") else '0';
 dice(2) <= NOT cnt(2);
-dice(3) <= '0' when (cnt = "10X") else '1';
+dice(3) <= '0' when (cnt = "100") else '1';
 
 end;
